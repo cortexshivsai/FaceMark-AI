ab129f6b07cb3817e633356e1e97b6daf624ed76
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-27 22:22:58 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,9 +1,33 @@
 import csv
+import mysql.connector
 from mysql.connector import Error
-
-from database import create_connection
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+
+# ========================================
+# DATABASE CONNECTION
+# ========================================
+
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+           host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+
+        if connection.is_connected():
+            print("MySQL database connected successfully!")
+            return connection
+
+    except Error as e:
+        print("Database connection error:", e)
+
+    return None
 
 
 # ========================================
